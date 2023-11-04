71a5eb6b36f31293016e28ef49c4c03ff4190d87
JoaquinSM13 <u201910511@upc.edu.pe>
2023-11-04 17:43:06 -0500

    update module statistics

--- src/app/views/statistics/statistics.module.ts
+++ src/app/views/statistics/statistics.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { StatisticsComponent } from "./statistics.component";
 import { StatisticsRoutingModule } from "./statistics-routing.module";
+import { GridModule } from "@coreui/angular";
 
 
 @NgModule({
   declarations: [ StatisticsComponent ],
   imports: [
     CommonModule,
-    StatisticsRoutingModule
+    StatisticsRoutingModule,
+    GridModule
   ]
 })
 export class StatisticsModule { }
