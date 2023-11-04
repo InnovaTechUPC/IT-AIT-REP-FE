5f8c4860d7b2d0be6c04023e4068215d7e0da534
JoaquinSM13 <u201910511@upc.edu.pe>
2023-11-04 17:41:35 -0500

    update module projects

--- src/app/views/projects/projects.module.ts
+++ src/app/views/projects/projects.module.ts
@@ -2,12 +2,28 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProjectsComponent } from "./projects.component";
 import { ProjectsRoutingModule } from "./projects-routing.module";
+import {
+  AvatarComponent,
+  ButtonDirective,
+  CardModule,
+  GridModule,
+  ProgressBarComponent,
+  ProgressComponent
+} from "@coreui/angular";
+import {IconDirective} from "@coreui/icons-angular";
 
 @NgModule({
   declarations: [ ProjectsComponent ],
   imports: [
     CommonModule,
-    ProjectsRoutingModule
+    ProjectsRoutingModule,
+    GridModule,
+    CardModule,
+    ButtonDirective,
+    IconDirective,
+    ProgressComponent,
+    ProgressBarComponent,
+    AvatarComponent
   ]
 })
 export class ProjectsModule { }
