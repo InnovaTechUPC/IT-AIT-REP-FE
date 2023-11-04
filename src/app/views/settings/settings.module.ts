f24efdf6635bf313a3a9ba6feb0fb5d74a29804e
JoaquinSM13 <u201910511@upc.edu.pe>
2023-11-04 17:42:23 -0500

    update module settings

--- src/app/views/settings/settings.module.ts
+++ src/app/views/settings/settings.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SettingsComponent } from "./settings.component";
 import { SettingsRoutingModule } from "./settings-routing.module";
+import { GridModule } from "@coreui/angular";
 
 @NgModule({
   declarations: [ SettingsComponent ],
   imports: [
     CommonModule,
-    SettingsRoutingModule
+    SettingsRoutingModule,
+    GridModule
   ]
 })
 export class SettingsModule { }
