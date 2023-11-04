39cb3015426298894d89ac963fa70ab0c243df41
JoaquinSM13 <u201910511@upc.edu.pe>
2023-11-04 17:40:55 -0500

    update module plans

--- src/app/views/plans/plans.module.ts
+++ src/app/views/plans/plans.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PlansComponent } from "./plans.component";
 import { PlansRoutingModule } from "./plans-routing.module";
+import { GridModule } from "@coreui/angular";
 
 @NgModule({
   declarations: [ PlansComponent ],
   imports: [
     CommonModule,
-    PlansRoutingModule
+    PlansRoutingModule,
+    GridModule
   ]
 })
 export class PlansModule { }
