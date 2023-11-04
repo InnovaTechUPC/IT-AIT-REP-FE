08fed7089984aea0b62cff647ed33c3b41cc5e33
JoaquinSM13 <u201910511@upc.edu.pe>
2023-11-04 17:39:13 -0500

    update module ai

--- src/app/views/ai/ai.module.ts
+++ src/app/views/ai/ai.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AiComponent } from "./ai.component";
 import { AiRoutingModule } from "./ai-routing.module";
+import { GridModule } from "@coreui/angular";
 
 @NgModule({
   declarations: [ AiComponent ],
   imports: [
     CommonModule,
-    AiRoutingModule
+    AiRoutingModule,
+    GridModule
   ]
 })
 export class AiModule { }
