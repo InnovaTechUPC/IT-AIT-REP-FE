c635dc233b04eca997622807850f90f2e6c8cad8
JoaquinSM13 <u201910511@upc.edu.pe>
2023-11-17 16:16:53 -0500

    projects module

--- src/app/views/projects/projects.component.ts
+++ src/app/views/projects/projects.component.ts
@@ -1,35 +1,6 @@
-import {Component, OnInit} from '@angular/core';
-
-const cards = [
-  {
-    title: 'Fintech Mobile App UI 1',
-    subtitle: '2023-07-10',
-    content: 'Fintech app development provides more freedom to banking and others financial institutions',
-    color: 'danger',
-    percent: 15
-  },
-  {
-    title: 'Fintech Mobile App UI 1',
-    subtitle: '2023-07-10',
-    content: 'Fintech app development provides more freedom to banking and others financial institutions',
-    color: 'warning',
-    percent: 39
-  },
-  {
-    title: 'Fintech Mobile App UI 1',
-    subtitle: '2023-07-10',
-    content: 'Fintech app development provides more freedom to banking and others financial institutions',
-    color: 'success',
-    percent: 40
-  },
-  {
-    title: 'Fintech Mobile App UI 1',
-    subtitle: '2023-07-10',
-    content: 'Fintech app development provides more freedom to banking and others financial institutions',
-    color: 'info',
-    percent: 33
-  }
-];
+import { Component, OnInit } from '@angular/core';
+import { ProjectService } from "../../services/project.service";
+import { ProjectBean } from "../../models/ProjectBean";
 
 @Component({
   selector: 'app-projects',
@@ -38,10 +9,20 @@ const cards = [
 })
 export class ProjectsComponent implements OnInit {
 
-  cards: any;
+  projects: ProjectBean[] = [];
+
+  constructor(private projectService: ProjectService) {
+
+  }
 
   ngOnInit(): void {
-    this.cards = cards;
+    this.loadTable();
+  }
+
+  loadTable(): void {
+    this.projectService.getAllProject().subscribe(pResponse=> {
+      this.projects = pResponse;
+    });
   }
 
 }
