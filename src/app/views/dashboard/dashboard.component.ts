ffce097c75d1e1bf5e30d8958365f16fe1f68103
JoaquinSM13 <u201910511@upc.edu.pe>
2023-11-17 16:15:43 -0500

    updated module dashboard

--- src/app/views/dashboard/dashboard.component.ts
+++ src/app/views/dashboard/dashboard.component.ts
@@ -3,52 +3,10 @@ import { MatTableDataSource } from "@angular/material/table";
 import { MatPaginator } from "@angular/material/paginator";
 import { MatSort } from "@angular/material/sort";
 import { ProjectBean } from "../../models/ProjectBean";
+import { ProjectService } from "../../services/project.service";
+// import { TaskBean } from "../../models/TaskBean";
 
-const projects: ProjectBean[] = [
-  {
-    id: 1,
-    name: 'Nelsa web developement',
-    dueDate: '2023-03-25',
-    status: 'Completed',
-    progress: 45
-  },
-  {
-    id: 2,
-    name: 'Datascale AI app ',
-    dueDate: '2023-10-01',
-    status: 'Delayed',
-    progress: 45
-  },
-  {
-    id: 3,
-    name: 'Media channel branding',
-    dueDate: '2023-05-22',
-    status: 'At risk',
-    progress: 45
-  },
-  {
-    id: 4,
-    name: 'Corlax iOS app develpoement',
-    dueDate: '2023-06-25',
-    status: 'On going',
-    progress: 45
-  },
-  {
-    id: 5,
-    name: 'Create a user flow of social application design',
-    dueDate: '2023-12-20',
-    status: 'Completed',
-    progress: 100
-  },
-  {
-    id: 6,
-    name: 'Website builder developement',
-    dueDate: '2024-04-15',
-    status: 'Completed',
-    progress: 45
-  }
-];
-
+/*
 const tasks = [
   {
     name: 'All',
@@ -69,9 +27,9 @@ const tasks = [
         checked: false
       }
     ]
-
   }
 ]
+*/
 
 @Component({
   templateUrl: 'dashboard.component.html',
@@ -79,28 +37,39 @@ const tasks = [
 })
 export class DashboardComponent implements OnInit {
 
-  tasks : any = [];
-  projects: any = [];
-  activeTabsIndex = 0;
-  displayedColumns: string[] = ['id', 'name', 'dueDate', 'status', 'progress'];
+  // tasks : TaskBean[] = [];
+  projects: ProjectBean[] = [];
+  displayedColumns: string[] = ['id', 'name', 'date', 'status', 'progress'];
   dataSource: MatTableDataSource<ProjectBean>;
+  // activeTabsIndex = 0;
+  isLoading = true;
 
   @ViewChild(MatPaginator) paginator!: MatPaginator;
   @ViewChild(MatSort) sort!: MatSort;
 
-
-  constructor() {
-    this.dataSource = new MatTableDataSource(projects);
+  constructor(
+    private projectService: ProjectService
+  ) {
+      this.dataSource = new MatTableDataSource<ProjectBean>([]);
   }
 
   ngOnInit(): void {
-    this.projects = projects;
-    this.tasks = tasks;
+    this.loadTable();
+    // this.tasks = tasks;
   }
 
-  ngAfterViewInit() {
-    this.dataSource.paginator = this.paginator;
-    this.dataSource.sort = this.sort;
+  loadTable(): void {
+    this.projectService.getAllProject().subscribe(response=> {
+        this.projects = response;
+        this.dataSource = new MatTableDataSource(this.projects);
+        this.isLoading = !(this.dataSource.data.length == 0);
+        this.updateSortAndPagination();
+    });
+  }
+
+  updateSortAndPagination(): void {
+      this.dataSource.paginator = this.paginator;
+      this.dataSource.sort = this.sort;
   }
 
   selectColor(status: string): string {
@@ -114,11 +83,11 @@ export class DashboardComponent implements OnInit {
     }
   }
 
-  onTabChange($event: number): void {
+/*  onTabChange($event: number): void {
     this.activeTabsIndex = $event;
-  }
+  }*/
 
-  applyFilter(event: Event) {
+  applyFilter(event: Event): void {
     const filterValue = (event.target as HTMLInputElement).value;
     this.dataSource.filter = filterValue.trim().toLowerCase();
 
