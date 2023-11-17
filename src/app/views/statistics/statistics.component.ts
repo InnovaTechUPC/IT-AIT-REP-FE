8ea84c2e547d159e2acc3d163590cd2f0b68f2f0
JoaquinSM13 <u201910511@upc.edu.pe>
2023-11-17 16:17:30 -0500

    module statistics added

--- src/app/views/statistics/statistics.component.ts
+++ src/app/views/statistics/statistics.component.ts
@@ -1,88 +1,13 @@
-import { Component, OnInit } from '@angular/core';
-
-const projects = [
-  {
-    id: 1,
-    name: 'Project',
-    progress: 45,
-    members: 30,
-    pendingTask: '0'
-  },
-  {
-    id: 2,
-    name: 'Project',
-    progress: 66,
-    members: 12,
-    pendingTask: '0'
-  },
-  {
-    id: 3,
-    name: 'Project',
-    progress: 33,
-    members: 5,
-    pendingTask: '0'
-  },
-  {
-    id: 4,
-    name: 'Project',
-    progress: 20,
-    members: 4,
-    pendingTask: '0'
-  },
-  {
-    id: 5,
-    name: 'Project',
-    progress: 22,
-    members: 2,
-    pendingTask: '0'
-  },
-  {
-    id: 6,
-    name: 'Project',
-    progress: 75,
-    members: 10,
-    pendingTask: '0'
-  }
-];
-
-const tasks = [
-  {
-    id: 1,
-    idProject: 2,
-    name: 'Task',
-    progress: 43
-  },
-  {
-    id: 2,
-    idProject: 3,
-    name: 'Task',
-    progress: 25
-  },
-  {
-    id: 3,
-    idProject: 1,
-    name: 'Task',
-    progress: 41
-  },
-  {
-    id: 4,
-    idProject: 2,
-    name: 'Task',
-    progress: 12
-  },
-  {
-    id: 5,
-    idProject: 6,
-    name: 'Task',
-    progress: 23
-  },
-  {
-    id: 6,
-    idProject: 5,
-    name: 'Task',
-    progress: 90
-  }
-];
+import {Component, OnInit, ViewChild} from '@angular/core';
+import { MatTableDataSource } from "@angular/material/table";
+import { TaskService } from "../../services/task.service";
+import { ProjectService } from "../../services/project.service";
+import { UserService } from "../../services/user.service";
+import { ProjectBean } from "../../models/ProjectBean";
+import { TaskBean } from "../../models/TaskBean";
+import { UserBean } from "../../models/UserBean";
+import {MatPaginator} from "@angular/material/paginator";
+import {MatSort} from "@angular/material/sort";
 
 const statisticsList = [
   {
@@ -95,49 +20,6 @@ const statisticsList = [
   }
 ]
 
-const members = [
-  {
-    name: 'Jane Cooper',
-    position: 'Support',
-    phone: '(225) 555-0118',
-    email: 'jane@microsoft.com',
-    status: 'active',
-    statics: ''
-  },
-  {
-    name: 'Floyd Miles',
-    position: 'Support',
-    phone: '(205) 555-0100',
-    email: 'floyd@yahoo.com',
-    status: 'inactive',
-    statics: ''
-  },
-  {
-    name: 'Ronald Richards',
-    position: 'Support',
-    phone: '(302) 555-0107',
-    email: 'ronald@adobe.com',
-    status: 'inactive',
-    statics: ''
-  },
-  {
-    name: 'Marvin McKinney',
-    position: 'Support',
-    phone: '(252) 555-0126',
-    email: 'marvin@tesla.com',
-    status: 'active',
-    statics: ''
-  },
-  {
-    name: 'Jerome Bell',
-    position: 'Support',
-    phone: '(629) 555-0129',
-    email: 'jerome@google.com',
-    status: 'active',
-    statics: ''
-  }
-];
-
 @Component({
   selector: 'app-statistics',
   templateUrl: './statistics.component.html',
@@ -146,34 +28,85 @@ const members = [
 export class StatisticsComponent  implements OnInit {
 
   statisticsList: any[] = [];
-  projects: any = [];
-  tasks: any[] = [];
-  members: any[] = [];
-
-  projectSelected: any = {}
+  projects: ProjectBean[] = [];
+  tasks: TaskBean[] = [];
+  users: UserBean[] = [];
+  projectSelected: ProjectBean;
   activeTabsIndex = 0;
+  displayedColumns: string[] = ['name', 'roleName', 'phone', 'email', 'status', 'statics'];
+  dataSource: MatTableDataSource<UserBean>;
+
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
+  constructor(
+      private projectService: ProjectService,
+      private taskService: TaskService,
+      private userService: UserService
+  ) {
+    this.dataSource = new MatTableDataSource<UserBean>([]);
+    this.projectSelected = {
+      id: 0,
+      name: '',
+      description: '',
+      date: '',
+      status: '',
+      progress: 0,
+      pendingTask: 0,
+      totalMembers: 0
+    };
+  }
 
   ngOnInit(): void {
-    this.projects = projects;
+    this.loadTaskData();
     this.statisticsList = statisticsList;
-    this.tasks = tasks.filter(x => x.idProject == 1);
-    projects.filter(x => {
-      if(x.id == 1) {
-        this.projectSelected = x;
+  }
+
+  loadTaskData(): void {
+    this.projectService.getAllProject().subscribe(pResponse=> {
+      this.projects = pResponse;
+      if(this.projects) {
+        this.taskService.getTasksByIdProject(this.projects.at(1)!.id).subscribe(tResponse => {
+          this.loadProjectAndTaskData(pResponse, tResponse!);
+        });
       }
     });
-    this.projectSelected.pendingTask = this.tasks.filter(x => x.value != 100).length.toString();
-    this.members = members;
+
+    this.userService.getAllUsers().subscribe(uResponse => {
+      this.users = uResponse;
+      this.dataSource = new MatTableDataSource(this.users);
+      this.updateSortAndPagination();
+    });
+  }
+
+  updateSortAndPagination(): void {
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
+  }
+
+  selectProject(id: number): void {
+    this.taskService.getTasksByIdProject(id).subscribe(tResponse => {
+      this.loadProjectAndTaskData(this.projects, tResponse, id);
+    });
+  }
+
+  loadProjectAndTaskData(projects: ProjectBean[], tasks: TaskBean[], id: number = 1): void {
+    this.tasks = tasks;
+    this.projectSelected = projects.find(x => x.id == id)!;
+    this.projectSelected.pendingTask = this.tasks.filter(x => !x.completed).length;
+  }
+
+  onTabChange($event: number): void {
+    this.activeTabsIndex = $event;
   }
 
   selectColor(status: string): string {
     switch (status) {
-      case 'active':
+      case 'Active':
       case 'Completed':
         return 'success';
       case 'On going':
       case 'Delayed': return 'warning';
-      case 'inactive':
+      case 'Inactive':
       case 'At risk':
         return 'danger';
       default:
@@ -181,17 +114,17 @@ export class StatisticsComponent  implements OnInit {
     }
   }
 
-  onTabChange($event: number): void {
-    this.activeTabsIndex = $event;
+  getProgressBar(task: TaskBean): string {
+    return ((task.hoursProgress/task.hoursTotal)*100).toFixed(2);
   }
 
-  selectProject(id: number): void {
-    this.tasks = tasks.filter(x => x.idProject == id);
-    projects.filter(x => {
-     if(x.id == id) {
-       this.projectSelected = x;
-     }
-    });
-    this.projectSelected.pendingTask = this.tasks.filter(x => x.value != 100).length.toString();
+  applyFilter(event: Event): void {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
   }
+
 }
